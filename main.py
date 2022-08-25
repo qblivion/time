0ad6c08e4c7dcacb54fd3f2d9458c3c69e612f4f
qblivion <71896680+qblivion@users.noreply.github.com>
2022-08-25 23:09:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import asyncio
+from pyrogram import Client
+from PIL import Image, ImageDraw, ImageFont
+from datetime import datetime
+
+
+api_id = 19890531
+api_hash = '7ec8158285a1d583eaea1d881c70ca2e'
+
+
+
+async def photo():
+    while True:
+        img = Image.open('12.jpg')
+        I1 = ImageDraw.Draw(img)
+        now = datetime.now()
+        current_time = now.strftime("%H:%M")
+        meFont = ImageFont.truetype('123.ttf', 40)
+        I1.text((400, 50), f'current time is: {current_time}', fill=(255, 0, 0), font=meFont)
+        img.save('123.jpg')
+        async with Client('myacc', api_id, api_hash) as app:
+
+            async for ph in app.get_chat_photos("me"):
+                asd = ph.file_id
+            # Get the photos to be deleted
+
+
+
+            # Delete one photo
+            await app.delete_profile_photos(asd)
+            await app.set_profile_photo(photo='123.jpg')
+        await asyncio.sleep(60)
+
+asyncio.get_event_loop().run_until_complete(photo())
